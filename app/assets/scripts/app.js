8692c9489e6ab37d9937efa48553107283fe498f
Arkadiusz Dziechciarz <a.dziechciarz@pl.vml.lan>
2019-02-14 16:21:23 +0100

    new

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    let my_text = $('#wrapper-16').find('p').text();
+ 
+    let new_text = [...my_text].map(function(e,i){
+        return e != ' ' ? `<span>${e}</span>` : ' ';
+    });
+
+    new_text = new_text.toString().replace(/,/g,'');
+
+    $('#wrapper-16').find('p').html(new_text);
+
+});
